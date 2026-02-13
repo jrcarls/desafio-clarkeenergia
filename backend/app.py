01dddf37ea99cb1b6e2e3f7695c72cf8b62352c7
Jean Reis <jeanreis.developer@gmail.com>
2026-02-13 12:26:35 -0300

    feat: adição do migrate e estrutura base para dados mock

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
