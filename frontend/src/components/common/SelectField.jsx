394cc11de10baa9450e01514a51b4fba49ce7a09
Jean Reis <jeanreis.developer@gmail.com>
2026-02-16 00:46:15 -0300

    refactor: melhorias na ui do select

--- frontend/src/components/common/SelectField.jsx
+++ frontend/src/components/common/SelectField.jsx
@@ -1,8 +1,20 @@
-import { Label, ListBox, Select } from "@heroui/react";
+import {
+  Autocomplete,
+  Description,
+  EmptyState,
+  Label,
+  ListBox,
+  SearchField,
+  useFilter,
+} from "@heroui/react";
+
 import { useAsyncList } from "@react-stately/data";
-import { Collection } from "react-aria-components";
+import { useState } from "react";
 
 export function StateSelect() {
+  const [selectedKey, setSelectedKey] = useState(null);
+  const { contains } = useFilter({ sensitivity: "base" });
+
   const list = useAsyncList({
     async load({ signal }) {
       const res = await fetch("http://127.0.0.1:5000/api/v1/estados", {
@@ -18,17 +30,37 @@ export function StateSelect() {
   });
 
   return (
-    <Select className="w-[256px]" placeholder="Selecione um estado">
+    <Autocomplete
+      className="w-[256px]"
+      placeholder="Selecione um estado"
+      selectionMode="single"
+      value={selectedKey}
+      onChange={setSelectedKey}
+    >
       <Label>Estado (UF)</Label>
 
-      <Select.Trigger>
-        <Select.Value />
-        <Select.Indicator />
-      </Select.Trigger>
+      <Autocomplete.Trigger>
+        <Autocomplete.Value />
+        <Autocomplete.ClearButton />
+        <Autocomplete.Indicator />
+      </Autocomplete.Trigger>
 
-      <Select.Popover>
-        <ListBox>
-          <Collection items={list.items}>
+      <Autocomplete.Popover>
+        <Autocomplete.Filter filter={contains}>
+          <SearchField autoFocus name="search" variant="secondary">
+            <SearchField.Group>
+              <SearchField.SearchIcon />
+              <SearchField.Input placeholder="Buscar estados..." />
+              <SearchField.ClearButton />
+            </SearchField.Group>
+          </SearchField>
+
+          <ListBox
+            items={list.items}
+            renderEmptyState={() => (
+              <EmptyState>Nenhum estado encontrado</EmptyState>
+            )}
+          >
             {(item) => (
               <ListBox.Item
                 key={item.id}
@@ -39,9 +71,13 @@ export function StateSelect() {
                 <ListBox.ItemIndicator />
               </ListBox.Item>
             )}
-          </Collection>
-        </ListBox>
-      </Select.Popover>
-    </Select>
+          </ListBox>
+        </Autocomplete.Filter>
+      </Autocomplete.Popover>
+
+      <Description>
+        Selecione o estado para buscar as soluções disponíveis
+      </Description>
+    </Autocomplete>
   );
 }
